3931d379e824931df0fda70aee7ee3127d5dbdc5
Harendra Chauhan <hChauhan4862@gmail.com>
2025-10-16 12:44:29 +0530

    Update grid layout in Dashboard component for consistency across screen sizes

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div className="relative h-screen overflow-hidden bg-primary-100 pb-10">
 <div className="mt-[360px] sm:mt-[360px] md:mt-[420px] min-h-[500px] max-h-[calc(100vh-360px)] sm:max-h-[calc(100vh-360px)] md:max-h-[calc(100vh-420px)] overflow-y-auto m-2 bg-background rounded-2xl pb-10">
       <ResponsiveContainer>
-          <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-4">
+          <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {dashboard.map((item) => (
               <DashboardCard
                 key={item.title+item.path}
